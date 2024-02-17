ddbdd46e7a631ece4e1870bd3e2ffb97d5728b23
GIgabyte <88191224+shalinithasha@users.noreply.github.com>
2024-02-17 21:37:40 +0530

    Update luna.py

--- luna.py
+++ luna.py
@@ -53,7 +53,7 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
+        "[GitHub](https://github.com/shalinithasha/LunaChatBot)"
         + " | [Group](t.me/PatheticProgrammers)",
         disable_web_page_preview=True,
     )
